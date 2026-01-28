989f236241c8f9ade52ec7456e68a68fce15aafa
Rafael Caiado <rafaelccaiado@gmail.com>
2026-01-28 15:04:02 -0300

    debug: final zero config attempt V6

--- src/components/OCRProcessing.jsx
+++ src/components/OCRProcessing.jsx
@@ -329,7 +329,7 @@ export default function OCRProcessing({ imageFile, selectedUnit, onComplete, onB
             )}
             {/* Debug Tag */}
             <div className="text-[8px] text-gray-300 text-right mt-4 uppercase">
-                Build: 2026-01-28-V5 (Routing Debug)
+                Build: 2026-01-28-V6 (Zero Config Fix)
             </div>
         </div>
     )
