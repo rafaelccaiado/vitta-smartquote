7c891266178f91f7ff66befb5c68138339eb1db6
Rafael Caiado <rafaelccaiado@gmail.com>
2026-01-28 17:50:16 -0300

    fix: V32 restore missing encoded_key variable

--- src/components/OCRProcessing.jsx
+++ src/components/OCRProcessing.jsx
@@ -328,8 +328,8 @@ export default function OCRProcessing({ imageFile, selectedUnit, onComplete, onB
                 </div>
             )}
             {/* Debug Tag */}
-            <div className="text-[8px] text-gray-300 text-right mt-4 uppercase text-blue-500 font-bold">
-                Build: 2026-01-28-V31 (JSON Sanitizer)
+            <div className="text-[8px] text-gray-300 text-right mt-4 uppercase text-green-600 font-bold">
+                Build: 2026-01-28-V32 (Variable Fixed)
             </div>
         </div>
     )
